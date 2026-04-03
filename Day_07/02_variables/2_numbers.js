fb67af27f71a58a850d33cd80c9c422fcf793e8f
Sanjeeb-J <sanjeebkodoth@gmail.com>
2026-04-03 20:22:29 +0530

    day 7

--- Day_07/02_variables/2_numbers.js
+++ Day_07/02_variables/2_numbers.js
@@ -4,6 +4,7 @@ let price = 10.99;
 let gpa = 2.1;
 
 console.log(typeof age);
+
 console.log(`You are ${age} years old`);
 console.log(`The price is $${price}`);
 console.log(`Your gpa is: ${gpa}`);
